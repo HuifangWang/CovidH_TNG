1c58cc72534e143e00cfc50d19f74fbed1699503
Jan Fousek <jan.fousek@univ-amu.fr>
2020-04-18 13:18:06 +0200

    Let's do this.

---
+++ src/data.py
@@ -0,0 +1,11 @@
+import os
+
+data_root = os.path.abspath(
+        os.path.join(
+            os.path.dirname(
+                os.path.dirname(__file__)
+            ),
+            'data'
+        )
+)
+
