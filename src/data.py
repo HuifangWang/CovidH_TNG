e4de65126021f0f0429d65ee987fb728a82fc174
Jan Fousek <jan.fousek@univ-amu.fr>
2020-04-19 10:27:01 +0200

    refactored basic data

--- src/data.py
+++ src/data.py
@@ -11,43 +11,14 @@ data_root = os.path.abspath(
 )
 
 def load_google_mobility_france():
-    df  = pd.read_csv(
-            os.path.join(data_root, 'external','Global_Mobility_Report.csv'),
+    return pd.read_csv(
+            os.path.join(data_root, 'processed','france_mobility_report.csv'),
             parse_dates=['date']
-    )
-
-    df_france = df.loc[
-        (df['country_region_code']=='FR') &
-        pd.isna(df['sub_region_1'])
-        , [
-            'date',
-            'retail_and_recreation_percent_change_from_baseline',
-            'grocery_and_pharmacy_percent_change_from_baseline',
-            'parks_percent_change_from_baseline',
-            'transit_stations_percent_change_from_baseline',
-            'workplaces_percent_change_from_baseline',
-            'residential_percent_change_from_baseline'
-        ]
-    ].set_index('date')
-
-    return df_france
+    ).set_index('date')
 
 def load_covid19_france():
-    df  = pd.read_csv(
+    return pd.read_csv(
             os.path.join(
-                data.data_root, 'external', 'datahub-covid-19', 'time-series-19-covid-combined_csv.csv'),
+                data_root, 'processed', 'france_covid19.csv'),
             parse_dates=['Date']
-    )
-
-    df_france = df.loc[
-            (df['Country/Region']=='France') &
-            pd.isna(df['Province/State']),
-            [
-                'Date',
-                'Confirmed',
-                'Recovered',
-                'Deaths'
-            ]
-    ].set_index('Date')
-
-
+    ).set_index('Date')
