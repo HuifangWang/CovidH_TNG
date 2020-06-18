0f61cf602d6bb70485d0e677f0b00cec2b046c54
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2020-06-18 11:21:25 +0200

    add simplified SDE model

---
+++ stan/beta-run.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pylab as pl
+import stanio
+import os
+import scipy.stats
+
+
+# the Stan run can sometimes get poorly initialized and
+# results are crap, just rerun it.
+cs = '~/crypt/cmdstan-2.22.1'
+os.system('./beta sample data file=beta.R output file=beta.csv')
+stanio.diagnose_csvs(cs, 'beta.csv')
+
+r = stanio.rload('beta.R')
+csv = stanio.parse_csv('beta.csv')
+
+alpha=1/14
+def ts_ci(v,ci=95,axis=0,it=None):
+    p = (100 - ci) / 2
+    a, b = np.percentile(v, [p, 100-p], axis=axis)
+    it = np.r_[:a.size] if it is None else it
+    pl.fill_between(it, a, b, alpha=0.5)
+
+
+pl.figure(figsize=(5,7))
+
+pl.subplot(311)
+pl.plot(r['r'])
+ts_ci(csv['gq_r'])
+pl.plot(r['iP'], r['P'])
+ts_ci(csv['gq_P'], it=r['iP'])
+pl.legend((
+    '$R_0(t)$', r'$\phi(t)$', r'$\hat{R_0}(t)$',
+     r'$\hat{\phi}(t)$'
+    ))
+pl.grid(1)
+
+pl.subplot(323)
+pl.hist(csv['a'], 30)
+pl.title('a~N(0,1)')
+pl.grid(1)
+
+pl.subplot(324)
+pl.hist(csv['s'], 30)
+pl.title('s~lN(0,1)')
+pl.grid(1)
+
+pl.subplot(325)
+pl.hist(csv['l'], 30)
+pl.title('l~lN(1,1)')
+pl.grid(1)
+
+pl.subplot(326)
+x = np.r_[:0.5:0.01]
+y = scipy.stats.lognorm.pdf(x,1,loc=0)
+pl.plot(x,y, 'k')
+pl.hist(csv['g'], 50, density=True)
+pl.legend((r'$p(\gamma)=\log N(0.1,1)$', r'$\hat{\gamma}$', ))
+# pl.title('g~lN(0.1,1)')
+pl.xlabel(r'$\gamma$')
+pl.title(r'$p(\gamma\|R0(t),\phi(t))$')
+pl.yticks(pl.yticks()[0], [])
+pl.grid(1)
+
+
+pl.tight_layout()
+pl.show()
+# pl.savefig('beta-run.png')
---
+++ stan/beta.stan
@@ -0,0 +1,55 @@
+data {
+  int nt; // 60 days starting March 10th
+  vector[nt] r; // RKI R 7 day now-cast
+  vector[nt] m; // Google mobility retail & recr / 100%
+  int iP[8];
+  vector[8] P;
+}
+
+transformed data {
+  real alpha = 1.0 / 14.0;
+}
+
+parameters {
+  vector[nt-1] z;
+  real a;
+  real g;
+  real l;
+  real s;
+}
+
+transformed parameters {
+  vector[nt] b = r*alpha;
+  vector[nt] p_ = rep_vector(b[1],nt);
+  vector[nt] p;
+  vector[nt] u = rep_vector(0,nt);
+  for (t in 1:nt-1) {
+    real Js = t*1.0/nt*0.13; // NY 40%, 2mo
+    real db = (b[1] + a*m[t] - Js + g*u[t]) - l*b[t];
+    real dp = (b[t] - p_[t] + u[t]*0.9)/3.0;
+    real du = (b[1] - b[t] - u[t])/21.0;
+    b[t+1] = b[t] + db + s*z[t];
+    p_[t+1] = p_[t] + dp;
+    u[t+1] = u[t] + du;
+  }
+  p = (p_ - b[1]) * 35;
+}
+
+model {
+  // priors
+  to_vector(z) ~ std_normal();
+  l ~ lognormal(1,1);
+  a ~ std_normal();
+  s ~ lognormal(0,1);
+  g ~ lognormal(0.1,1);
+  // condition on data
+  r ~ normal(b/alpha,s);
+  P ~ normal(p[iP],sqrt(s));
+}
+
+generated quantities {
+  vector[nt] gq_r;
+  vector[8] gq_P;
+  for (t in 1:nt) gq_r[t] = normal_rng(b[t]/alpha,s);
+  for (t in 1: 8) gq_P[t] = normal_rng(p[iP[t]], sqrt(s));
+}
