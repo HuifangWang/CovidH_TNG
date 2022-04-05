59777e67df6ed3d3b432ce7e21625564fb67bcdc
Jan Fousek <jan.fousek@univ-amu.fr>
2022-04-05 23:30:44 +0200

    ihme cases supplementary figure

---
+++ data/external/ihme/get.sh
@@ -0,0 +1,3 @@
+wget https://ihmecovid19storage.blob.core.windows.net/latest/data_download_file_reference_2020.csv
+wget https://ihmecovid19storage.blob.core.windows.net/latest/data_download_file_reference_2021.csv
+wget https://ihmecovid19storage.blob.core.windows.net/latest/data_download_file_reference_2022.csv
